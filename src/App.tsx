aad604a35e78db7ea6d2ee920eca2a092562dd91
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-04 13:35:51 +0300

    lesson 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,115 @@
 import React from 'react';
 import './App.css';
 
+// Тип для элементов массива
+type ArrayType = {
+    albumId: number
+    id: number
+    title: string
+    url: string
+    thumbnailUrl: string
+}
+
 function App() {
-  return (
-    <div className="App">
 
-      <div>Hello Max</div>
-      <div>Hello Max</div>
+    const array: ArrayType[] = [
+        {
+            "albumId": 1,
+            "id": 1,
+            "title": "accusamus beatae ad facilis cum similique qui sunt",
+            "url": "https://via.placeholder.com/600/92c952",
+            "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+        },
+        {
+            "albumId": 1,
+            "id": 2,
+            "title": "reprehenderit est deserunt velit ipsam",
+            "url": "https://via.placeholder.com/600/771796",
+            "thumbnailUrl": "https://via.placeholder.com/150/771796"
+        },
+        {
+            "albumId": 1,
+            "id": 3,
+            "title": "officia porro iure quia iusto qui ipsa ut modi",
+            "url": "https://via.placeholder.com/600/24f355",
+            "thumbnailUrl": "https://via.placeholder.com/150/24f355"
+        },
+        {
+            "albumId": 1,
+            "id": 4,
+            "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+            "url": "https://via.placeholder.com/600/d32776",
+            "thumbnailUrl": "https://via.placeholder.com/150/d32776"
+        },
+        {
+            "albumId": 1,
+            "id": 5,
+            "title": "natus nisi omnis corporis facere molestiae rerum in",
+            "url": "https://via.placeholder.com/600/f66b97",
+            "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
+        },
+        {
+            "albumId": 1,
+            "id": 6,
+            "title": "accusamus ea aliquid et amet sequi nemo",
+            "url": "https://via.placeholder.com/600/56a8c2",
+            "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
+        },
+        {
+            "albumId": 1,
+            "id": 7,
+            "title": "officia delectus consequatur vero aut veniam explicabo molestias",
+            "url": "https://via.placeholder.com/600/b0f7cc",
+            "thumbnailUrl": "https://via.placeholder.com/150/b0f7cc"
+        },
+        {
+            "albumId": 1,
+            "id": 8,
+            "title": "aut porro officiis laborum odit ea laudantium corporis",
+            "url": "https://via.placeholder.com/600/54176f",
+            "thumbnailUrl": "https://via.placeholder.com/150/54176f"
+        },
+        {
+            "albumId": 1,
+            "id": 9,
+            "title": "qui eius qui autem sed",
+            "url": "https://via.placeholder.com/600/51aa97",
+            "thumbnailUrl": "https://via.placeholder.com/150/51aa97"
+        },
+        {
+            "albumId": 1,
+            "id": 10,
+            "title": "beatae et provident et ut vel",
+            "url": "https://via.placeholder.com/600/810b14",
+            "thumbnailUrl": "https://via.placeholder.com/150/810b14"
+        },
+        {
+            "albumId": 1,
+            "id": 11,
+            "title": "nihil at amet non hic quia qui",
+            "url": "https://via.placeholder.com/600/1ee8a4",
+            "thumbnailUrl": "https://via.placeholder.com/150/1ee8a4"
+        },
+    ]
 
-    </div>
-  );
+
+    console.log(array)
+    return (
+        <div className="App">
+            <div>Hello Max</div>
+            <div>Hello Max</div>
+            <h1>Lesson IT</h1>
+
+            {array.map((ar) => (
+                <div key={ar.id}>
+                    <h3>{ar.title}</h3>
+                    <a href={ar.url} target='_blank' rel='noopener noreferrer'>
+                        <img src={ar.thumbnailUrl} alt={ar.title}/>
+                    </a>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
